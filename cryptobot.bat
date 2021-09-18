6431bac19bb0bc987f757f1cb6fed51a67a7dabf
mshivanna <manojshivanna@gmail.com>
2021-09-18 23:45:31 +1000

    enabled live trading

--- cryptobot.bat
+++ cryptobot.bat
@@ -1,3 +1,3 @@
 cd "C:\Users\manoj\OneDrive\Dev\cryptobot\pycryptobot\"
-python3 pycryptobot.py --market BTCAUD --granularity 3600 --sim fast --verbose 0
+python3 pycryptobot.py --market BTCAUD --granularity 3600 --verbose 0
 pause
