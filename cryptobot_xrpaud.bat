35a0f95b5056d577f447f78c79cc549476157110
mshivanna <manojshivanna@gmail.com>
2021-09-19 01:05:47 +1000

    XRP added

---
+++ cryptobot_xrpaud.bat
@@ -0,0 +1,6 @@
+@echo off 
+Title “Digital Think Tank (DTT) RippleNet (XRP) Bot”
+cd "C:\Users\manoj\OneDrive\Dev\cryptobot\pycryptobot\"
+python3 pycryptobot.py --market XRPAUD --verbose 0
+@echo on
+pause
